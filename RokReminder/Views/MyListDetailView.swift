4e94b5c0335139c493a82e300e45827e8c9e236a
Rokku <quocloctran357@gmail.com>
2024-05-30 16:23:44 +0700

    listDetail

---
+++ RokReminder/Views/MyListDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MyListDetailView.swift
+//  RokReminder
+//
+//  Created by Quốc Lộc on 30/05/2024.
+//
+
+import SwiftUI
+
+struct MyListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyListDetailView()
+}
